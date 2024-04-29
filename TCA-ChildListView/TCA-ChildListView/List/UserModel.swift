53f763e324224dbc1bd15738f0c971f93bfcc416
JeongDeokho <ekdj98@gmail.com>
2024-04-29 11:33:00 +0900

    commit

---
+++ TCA-ChildListView/TCA-ChildListView/List/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  TCA-ChildListView
+//
+//  Created by Jeong Deokho on 2024/04/29.
+//
+
+import Foundation
+
+struct UserModel: Equatable, Identifiable {
+    var id = UUID()
+    var name = ""
+}
